d1b305ff110a3d1ed237ddfe27dacb847df86ee0
Benjamin Caldwell <b.caldwell@salesforce.com>
2020-02-20 10:03:09 -0800

    move zplug to zinit for faster loading

---
+++ zsh/includes/version_managers.zsh
@@ -0,0 +1,13 @@
+
+#nvm
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+#export NVM_LAZY_LOAD=true
+#zplug "lukechilds/zsh-nvm"
+
+# gvm
+# [[ -s "/home/benjamin/.gvm/scripts/gvm" ]] && source "/home/benjamin/.gvm/scripts/gvm"
+
+# rvm
+# [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+# [[ -s "$HOME/.rvm/scripts/rvm" ]] && export PATH="$PATH:$HOME/.rvm/bin"
